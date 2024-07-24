96085b12b9767fed0a3456cc5a8aef0b024d5d41
Geobert Quach <geobert@proton.me>
2024-07-24 22:07:34 +0200

    disable hints by default (<leader>uh to toggle them)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,17 +1,23 @@
 return {
-  -- require("plugins.lspsaga"),
-  {
-    "aznhe21/actions-preview.nvim",
-    opts = function()
-      return {
-        diff = {
-          algorithm = "patience",
-          ignore_whitespace = true,
+    -- require("plugins.lspsaga"),
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            inlay_hints = { enabled = false },
         },
-        telescope = require("telescope.themes").get_dropdown({ winblend = 5 }),
-      }
-    end,
-  },
-  "antosha417/nvim-lsp-file-operations",
-  "onsails/lspkind.nvim",
+    },
+    {
+        "aznhe21/actions-preview.nvim",
+        opts = function()
+            return {
+                diff = {
+                    algorithm = "patience",
+                    ignore_whitespace = true,
+                },
+                telescope = require("telescope.themes").get_dropdown({ winblend = 5 }),
+            }
+        end,
+    },
+    "antosha417/nvim-lsp-file-operations",
+    "onsails/lspkind.nvim",
 }
