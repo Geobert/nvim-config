3655fd9f9d187bfe752a9335674b9e2150d84322
Geobert Quach <geobert.quach@isode.com>
2024-12-16 09:58:24 +0100

    office version update

--- lua/plugins/bacon.lua
+++ lua/plugins/bacon.lua
@@ -1,6 +1,14 @@
 return {
     {
         "Canop/nvim-bacon",
+        config = function()
+            require("bacon").setup({
+                quickfix = {
+                    enabled = true, -- Enable Quickfix integration
+                    event_trigger = true, -- Trigger QuickFixCmdPost after populating Quickfix list
+                },
+            })
+        end,
         opts = {},
     },
 }
